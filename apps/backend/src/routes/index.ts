779c4b95e273109dc4f579412f83a3c1643f8f1a
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2025-11-03 03:39:41 +0000

    feat: add /otp routes use in route index

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import routerContrato from '@routes/contratos/contratos.ts';
-import { handleError } from '@middlewares/index.ts';
+import routerOtp from '@routes/otp/otp.ts';
+import { handleError, requestLogger } from '@middlewares/index.ts';
 
 const router = Router();
 
+router.use(requestLogger);
 router.use('/contratos', routerContrato);
+router.use('/otp', routerOtp);
 
 router.use(handleError);
 
