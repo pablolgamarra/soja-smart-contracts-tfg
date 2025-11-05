889769ec3e2a3cb7dc73d83e220a6cd5a10e8bc2
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-11-05 13:42:53 +0000

    feat!: update middlewares, add request and response logger

--- apps/backend/src/middlewares/index.ts
+++ apps/backend/src/middlewares/index.ts
@@ -1,11 +1,70 @@
 import { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
 
-export const requestLogger = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log(`${req.method}. TIME: ${new Date(Date.now())}. Host: ${req.host}. Path: ${req.url}`)
+dotenv.config();
+
+// Imprimir logs para registro de solicitudes
+export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(`${req.method} Request. TIME: ${new Date(Date.now())}. Host: ${req.host}. Path: ${req.originalUrl}`)
     next();
 }
 
-//Error handling middleware
+// Imprimir logs para registro de respuestas
+export const responseLogger = (req: Request, res: Response, next: NextFunction) => {
+    const startTime = Date.now();
+    let alreadyLogged = false;
+
+    const log = (body: any) => {
+        if(alreadyLogged) return;
+
+        alreadyLogged = true;
+
+
+        const duration = Date.now() - startTime;
+        console.log(`[${res.statusCode}] ${req.method} ${req.originalUrl} (${duration}ms)`);
+        
+        if (process.env.NODE_ENV !== "production") {
+            try {
+
+                console.log("Response:", JSON.stringify(body, null, 2));
+            } catch {
+                console.log("Response (non-JSON)");
+            }
+        }
+    }
+
+    // Guardar referencia original a res.json o res.send
+    const originalJson = res.json.bind(res);
+    const originalSend = res.send.bind(res);
+
+    // Sobrescribir res.json
+    res.json = (body: any) => {
+        log(body);
+        return originalJson(body);
+    };
+
+
+    // Sobrescribir res.send (para respuestas que no usan JSON)
+    res.send = (body: any) => {
+        log(body);
+        return originalSend(body);
+    };
+
+    next();
+};
+
+// Validador de solicitudes que deberían usar JSON
+export const validateJSONReqs = (req:Request, res:Response, next:NextFunction) => {
+    if (req.method === 'POST' || req.method === 'PUT' || req.method === 'DELETE') {
+        if (req.get('Content-Type') !== 'application/json') {
+            return res.status(400).json({ error: 'Content-Type must be application/json' });
+        }
+    }
+
+    next();
+};
+
+//Manejar errores
 export const handleError = (err:Error, req:Request, res:Response, next:NextFunction) => {
     console.error('API ERROR', err);
 
