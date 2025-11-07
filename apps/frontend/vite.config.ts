f2a0b0082f2116fd407019fad93d8d998f5a2e6e
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-11-07 20:02:59 +0000

    feat: add alias paths in frontend

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,14 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: '127.0.0.1',
-    proxy: {
-        '/api': 'http://localhost:3000'
+    plugins: [react(), tailwindcss()],
+    server: {
+        host: '127.0.0.1',
+        proxy: {
+            '/api': 'http://localhost:3000'
+        }
+    },
+    resolve:{
+        alias: {
+            "@api": path.resolve(__dirname, "src/api"),
+            "@components": path.resolve(__dirname, "src/components"),
+            "@constants": path.resolve(__dirname, "src/constants"),
+            "@context": path.resolve(__dirname, "src/context"),
+            "@helpers": path.resolve(__dirname, "src/helpers"),
+            "@hooks": path.resolve(__dirname, "src/hooks"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+            "@routes": path.resolve(__dirname, "src/routes"),
+            "@types": path.resolve(__dirname, "src/types")
+        }
     }
-  }
 })
