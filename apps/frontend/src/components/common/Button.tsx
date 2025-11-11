0e345a9cc7d970374f264006c8292820f8c3e47c
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-11-11 20:08:22 +0000

    feat: update button component props

--- apps/frontend/src/components/common/Button.tsx
+++ apps/frontend/src/components/common/Button.tsx
@@ -2,13 +2,15 @@ import type { PropsWithChildren } from "react";
 import type React from "react";
 
 export interface IButtonProps {
-    onClick?: ()=>void;
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>)=>void;
     className?: string;
+    disabled?: boolean;
 }
 
-const Button: React.FC<PropsWithChildren<IButtonProps>> = ({children, onClick, className})=>{
+const Button: React.FC<PropsWithChildren<IButtonProps>> = ({children, disabled, onClick, className})=>{
     return (
         <button
+            disabled={disabled}
             onClick={onClick}
             className={className || "px-4 py-2 bg-blue-600 text-white rounded-lg"}
         >
