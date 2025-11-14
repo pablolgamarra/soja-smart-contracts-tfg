6d35ff2b6561e124ad8523ab10972d3498f72e89
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-11-14 20:22:20 +0000

    feat: typify backend blockchain connection

--- apps/backend/src/blockchain/BlockchainConnection.ts
+++ apps/backend/src/blockchain/BlockchainConnection.ts
@@ -1,6 +1,6 @@
 import { Contract, ethers, JsonRpcProvider, Wallet } from "ethers";
 import { getEnv } from "src/helpers/index.ts";
-import { CONTRACT_ABI } from "./abis/ContratoGranosSoja.ts";
+import {ContratoGranosSoja, ContratoGranosSoja__factory} from "./types/index.ts";
 
 const CONFIG = {
     RPC: getEnv("RPC_URL"),
@@ -11,15 +11,18 @@ const CONFIG = {
 export class BlockchainConnection {
     readonly rpcProvider: JsonRpcProvider;
     readonly relayerWallet: Wallet;
-    readonly contratoView: Contract;
-    readonly contratoRelayer: Contract;
+    readonly contratoView: ContratoGranosSoja
+    readonly contratoRelayer: ContratoGranosSoja;
 
     constructor() {
         // Inicializar provider y contrato
         this.rpcProvider = new ethers.JsonRpcProvider(CONFIG.RPC);
         this.relayerWallet = new ethers.Wallet(CONFIG.RELAYER_KEY, this.rpcProvider);
-        this.contratoView = new ethers.Contract(CONFIG.CONTRACT_ADDRESS, CONTRACT_ABI, this.rpcProvider);
-        this.contratoRelayer = new ethers.Contract(CONFIG.CONTRACT_ADDRESS, CONTRACT_ABI, this.relayerWallet);
+        
+        // this.contratoView = new ethers.Contract(CONFIG.CONTRACT_ADDRESS, CONTRACT_ABI, this.rpcProvider);
+        // this.contratoRelayer = new ethers.Contract(CONFIG.CONTRACT_ADDRESS, CONTRACT_ABI, this.relayerWallet);
+        this.contratoView = ContratoGranosSoja__factory.connect(CONFIG.CONTRACT_ADDRESS, this.rpcProvider);
+        this.contratoRelayer = ContratoGranosSoja__factory.connect(CONFIG.CONTRACT_ADDRESS, this.relayerWallet);
     }
 
     /**
