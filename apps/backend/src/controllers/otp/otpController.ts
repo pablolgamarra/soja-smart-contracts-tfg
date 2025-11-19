075e2a2373e9bb4a6cdb71b3ef1ff634e550e5ee
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2025-11-19 05:35:50 +0000

    feat: update otpController

--- apps/backend/src/controllers/otp/otpController.ts
+++ apps/backend/src/controllers/otp/otpController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { otpService } from '@services/otpService.ts';
 import notificatorService from '@services/notifications/notificationService.ts';
 import { getEnv } from '@helpers/index.ts';
@@ -8,55 +8,73 @@ const CONFIG = {
 }
 
 class OTPController {
-    // Crear OTP
-    public generarOTP = async (req: Request, res: Response) => {
-        const { contractId, email, number } = req.body;
-
-        if (!contractId) {
-            return res.status(400).json({ success: false, message: "Datos incompletos" });
-        }
-
+    // Crear OTP y enviar notificaciones
+    public generarOTP = async (req: Request, res: Response, next: NextFunction) => {
         try {
+            const { contractId, email, number } = req.body;
+
+            if (!contractId) {
+                return res.status(400).json({ success: false, message: "Datos incompletos" });
+            }
+
             const sellerAddress = CONFIG.relayer 
 
             // Delegar la lógica de negocio a otpService
-            const otpResult = await otpService.generarOTP(contractId, sellerAddress);
+            const otpCode = await otpService.generarOTP({id: contractId, billeteraVendedor: sellerAddress});
             
             // Enviar OTP por correo y/o WhatsApp
-            await notificatorService.sendOTPNotification({email, number}, otpResult.otp);
+            await notificatorService.sendOTPNotification({email, number}, otpCode);
 
-            res.json({
+            return res.json({
                 success: true,
-                message: "OTP generado.",
-                otp: otpResult.otp, // opcional, si quieres enviar el OTP generado
+                message: "OTP generado",
+                data: otpCode
             });
-        } catch (error) {
-            console.error("❌ Error generando OTP:", error);
-            res.status(500).json({ success: false, message: "Error al generar y enviar el OTP." });
+        } catch (e) {
+            next(e);
         }
     };
 
     // Verificar OTP
-    public verificarOTP = async (req: Request, res: Response) => {
-        const { contractId, otp } = req.body;
-
-        const sellerAddress = CONFIG.relayer 
-
-        if (!contractId || !otp) {
-            return res.status(400).json({ success: false, message: "Datos incompletos" });
-        }
-
+    public verificarOTP = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            // Delegar la lógica de verificación de OTP a otpService
-            const verificationResult = await otpService.verificarOTP(contractId, sellerAddress, otp);
+            const { contractId, otp } = req.body;
+
+            const sellerAddress = CONFIG.relayer 
+
+            if (!contractId || !otp) {
+                return res.status(400).json({ success: false, message: "Datos incompletos" });
+            }
+
+            // Verificar OTP usando servicio
+            const checkOtp = await otpService.verificarOTP({ id: contractId, billeteraVendedor: sellerAddress }, otp);
+
+            if (checkOtp.message === 'OTP no encontrado') {
+                return res.status(404).json({ success:false, message: "OTP no encontrado" });
+            }
+
+            if (checkOtp.message === 'OTP incorrecto') {
+                return res.status(401).json({ success: false, message: "OTP incorrecto" });
+            }
+
+            if (checkOtp.message === 'OTP expirado') {
+                return res.status(410).json({ success: false, message: "OTP expirado" });
+            }
+
+            if (checkOtp.message === 'OTP ya utilizado') {
+                return res.status(409).json({ success: false, message: "OTP ya utilizado" });
+            }
+
+            if (!checkOtp.valid) {
+                return res.status(400).json({ success: false, message: "Error verificando OTP" });
+            }
 
             res.json({
                 success: true,
                 message: "OTP verificado correctamente. El vendedor puede firmar el contrato.",
             });
-        } catch (error) {
-            console.error("❌ Error verificando OTP:", error);
-            res.status(500).json({ success: false, message: "Error al verificar el OTP." });
+        } catch (e) {
+            next(e);
         }
     }
 }
