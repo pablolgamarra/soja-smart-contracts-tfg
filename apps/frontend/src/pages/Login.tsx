42515d54b8982237bd4121cfe4b8386895994a2e
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-11-28 17:52:05 +0000

    feat: update Login page to use toasts

--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -1,8 +1,18 @@
 import { useWeb3Context } from "@hooks/useWeb3Context";
 import Button from "@components/common/Button";
+import { useToast } from "@hooks/useToast";
 
 export default function Login() {
     const { connectWallet } = useWeb3Context();
+    const { addToast } = useToast();
+
+    const handleConnectWallet = async () => {
+        try {
+            await connectWallet();
+        } catch (error) {
+            addToast((error as Error).message, "error");
+        }
+    }
 
     return (
         <main className="min-h-screen w-full bg-gray-900 text-gray-100 flex flex-col items-center justify-center p-10 transition-colors duration-500">
@@ -11,7 +21,7 @@ export default function Login() {
             </h1>
             <div className="flex flex-col mx-auto min-w-6/12 rp-6 border rounded-lg shadow-md p-7">
                 <h2 className="text-xl font-semibold mb-12">Se debe conectar con una Wallet Metamask para continuar</h2>
-                <Button variant="primary" onClick={connectWallet} >Conectar a Metamask</Button>
+                <Button variant="primary" onClick={handleConnectWallet} >Conectar a Metamask</Button>
             </div>
         </main>
     );
