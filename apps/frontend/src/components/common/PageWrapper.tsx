2ee9dbb699ba8e7ba50eddeb8ba42a2ce0f79e01
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-11-28 20:15:15 +0000

    feat: remove metainfo from navbar

--- apps/frontend/src/components/common/PageWrapper.tsx
+++ apps/frontend/src/components/common/PageWrapper.tsx
@@ -1,11 +1,8 @@
 import { Fragment, type PropsWithChildren } from "react"
 import Navbar from "@components/common/Navbar";
 import Footer from "@components/common/Footer";
-import { useWeb3Context } from "@hooks/useWeb3Context";
 
-const PageWrapper:React.FC<PropsWithChildren> = ({children}) => {
-    const web3Context = useWeb3Context();
-    
+const PageWrapper:React.FC<PropsWithChildren> = ({children}) => {    
     return (
         <Fragment>
             <main className="min-h-screen w-full bg-gray-900 text-gray-100 flex flex-col items-center justify-center p-10 transition-colors duration-500">
@@ -13,16 +10,6 @@ const PageWrapper:React.FC<PropsWithChildren> = ({children}) => {
                     🌾 Smart — Contratos Inteligentes de Compra-Venta de Granos
                 </h1>
                 <Navbar />
-                    <p className="mb-3">
-                        ✅ Conectado como:{" "}
-                        <span className="font-mono text-sm">{web3Context.userAddress}</span>
-                    </p>
-                    <p>
-                        📜 Contrato activo:{" "}
-                        <span className="font-mono text-sm">
-                            {web3Context.deployedContract?.target?.toString()}
-                        </span>
-                    </p>
                     {children}
                 <Footer />
             </main>
