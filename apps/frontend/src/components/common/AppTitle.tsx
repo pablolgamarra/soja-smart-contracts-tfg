f108c57aec0bcbfaf11a86b3f2c787ab6c22d0b8
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-12-09 17:18:13 +0000

    feat: move title to separate component

---
+++ apps/frontend/src/components/common/AppTitle.tsx
@@ -0,0 +1,9 @@
+import type React from "react";
+
+const AppTitle: React.FC = () => (
+    <h1 className="text-4xl md:text-5xl font-extrabold mb-12 text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-400 text-center drop-shadow-lg">
+        🌾 Smart Contracts de Compra-Venta de Granos
+    </h1>
+);
+
+export default AppTitle;
