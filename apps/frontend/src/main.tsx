da0d2c4a488480871fe8d51aee6bd37e758d228d
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-12-09 17:12:14 +0000

    feat: update app entrypoint (main.tsx)

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -6,7 +6,12 @@ import { Web3ContextProvider } from './context/Web3Context.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ToastProvider } from '@context/ToastContext.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const documentRoot = document.getElementById('root');
+
+if(!documentRoot) throw new Error("Root element not found");
+
+// Envuelve toda la app con los providers necesarios. Este es el punto de entrada
+createRoot(documentRoot).render(
   <StrictMode>
     <Web3ContextProvider>
         <ToastProvider>
