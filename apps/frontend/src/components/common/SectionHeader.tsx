5f7d8afcf7250817e308c6a987208ee6c7de10ed
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-12-09 20:15:35 +0000

    feat: update styles of SectionHeader

--- apps/frontend/src/components/common/SectionHeader.tsx
+++ apps/frontend/src/components/common/SectionHeader.tsx
@@ -7,11 +7,13 @@ export interface IPageHeaderProps {
 
 const SectionHeader: React.FC<IPageHeaderProps> = ({title, description}: IPageHeaderProps) => {
     return (
-        <div className="bg-gradient-to-r from-gray-800 to-gray-900 rounded-xl shadow-2xl p-8 mb-6 border border-gray-700">
-            <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-400 mb-2">
+        <div className="bg-gradient-to-r from-gray-800 to-gray-900 rounded-xl p-8 shadow-xl border border-gray-700">
+            <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-500 via-blue-400 to-purple-400 drop-shadow">
                 {title}
             </h1>
-            <p className="text-gray-400 text-lg">{description}</p>
+            <p className="text-gray-400 text-lg mt-2">
+                {description}
+            </p>
         </div>
     );
 }
