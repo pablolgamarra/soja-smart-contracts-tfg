823a0740ee46ab61a585decc1051fffc76018605
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-12-09 17:17:38 +0000

    feat: refactor Login page

--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -1,28 +1,31 @@
 import { useWeb3Context } from "@hooks/useWeb3Context";
-import Button from "@components/common/Button";
 import { useToast } from "@hooks/useToast";
+import LoginCard from "@components/auth/LoginCard";
+import { useState } from "react";
+import AuthLayout from "@components/layouts/AuthLayout";
 
 export default function Login() {
     const { connectWallet } = useWeb3Context();
     const { addToast } = useToast();
 
+    const [ isConnecting, setIsConnecting ] = useState<boolean>(false);
+
     const handleConnectWallet = async () => {
+        setIsConnecting(true);
         try {
             await connectWallet();
         } catch (error) {
-            addToast((error as Error).message, "error");
+            addToast((error as Error).message || "Conexión rechazada o fallida.", "error");
+            setIsConnecting(false);
         }
     }
 
     return (
-        <main className="min-h-screen w-full bg-gray-900 text-gray-100 flex flex-col items-center justify-center p-10 transition-colors duration-500">
-            <h1 className="text-4xl md:text-5xl font-extrabold mb-8 text-green-400 text-center animate-pulse">
-                🌾 Smart — Contratos Inteligentes de Compra-Venta de Granos
-            </h1>
-            <div className="flex flex-col mx-auto min-w-6/12 rp-6 border rounded-lg shadow-md p-7">
-                <h2 className="text-xl font-semibold mb-12">Se debe conectar con una Wallet Metamask para continuar</h2>
-                <Button variant="primary" onClick={handleConnectWallet} >Conectar a Metamask</Button>
-            </div>
-        </main>
+        <AuthLayout>
+            <LoginCard
+                onConnect={handleConnectWallet}
+                isConnecting={isConnecting}
+            />
+        </AuthLayout>
     );
 }
